0ded919f01c0283a1bd7649b10a8a7553d8e8f35
computerKomputer <computerenggineringclub@gmail.com>
2026-09-22 12:16:06 +0800

    Add temporary Vercel error diagnostics

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('log_errors', '1');
+error_reporting(E_ALL);
+
+register_shutdown_function(function () {
+    $error = error_get_last();
+    if ($error !== null) {
+        error_log('FATAL PHP: ' . json_encode($error));
+    }
+});
+
 require __DIR__ . '/../public/index.php';
