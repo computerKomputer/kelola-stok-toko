c32d45fd9c96519ae4ecb90552e7fc77cadf67b7
computerKomputer <computerenggineringclub@gmail.com>
2026-09-22 12:52:32 +0800

    Improve Vercel exception diagnostics

--- api/index.php
+++ api/index.php
@@ -5,10 +5,14 @@ ini_set('display_startup_errors', '1');
 ini_set('log_errors', '1');
 error_reporting(E_ALL);
 
+set_exception_handler(function (Throwable $e) {
+    error_log('VERCEL_EXCEPTION: ' . get_class($e) . ' | ' . $e->getMessage() . ' | ' . $e->getFile() . ':' . $e->getLine());
+});
+
 register_shutdown_function(function () {
     $error = error_get_last();
     if ($error !== null) {
-        error_log('FATAL PHP: ' . json_encode($error));
+        error_log('VERCEL_FATAL: ' . json_encode($error));
     }
 });
 
