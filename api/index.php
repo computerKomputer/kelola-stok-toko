deb7593753fc6ac44d9cec37f16bc1212d20062f
computerKomputer <computerenggineringclub@gmail.com>
2026-09-23 08:44:48 +0800

    Add Vercel fatal error diagnostics

--- api/index.php
+++ api/index.php
@@ -1,19 +1,22 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+ini_set('display_errors', '0');
 ini_set('log_errors', '1');
 error_reporting(E_ALL);
 
-set_exception_handler(function (Throwable $e) {
-    error_log('VERCEL_EXCEPTION: ' . get_class($e) . ' | ' . $e->getMessage() . ' | ' . $e->getFile() . ':' . $e->getLine());
-});
-
 register_shutdown_function(function () {
     $error = error_get_last();
-    if ($error !== null) {
-        error_log('VERCEL_FATAL: ' . json_encode($error));
+
+    if ($error && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR], true)) {
+        error_log(
+            'CAKRAWALA_FATAL: ' .
+            $error['message'] .
+            ' | FILE: ' . $error['file'] .
+            ' | LINE: ' . $error['line']
+        );
     }
 });
+
+error_log('CAKRAWALA_START: PHP=' . PHP_VERSION);
 
 require __DIR__ . '/../public/index.php';
