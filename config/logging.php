af8b8a57432d802537fcec9a31eb37dabb4988b0
computerKomputer <computerenggineringclub@gmail.com>
2026-09-23 09:45:52 +0800

    Fix Laravel logging configuration for Vercel

--- config/logging.php
+++ config/logging.php
@@ -1,2 +1,22 @@
 <?php
-return ['default'=>'single','channels'=>['single'=>['driver'=>'single','path'=>storage_path('logs/laravel.log'),'level'=>'debug'],'stderr'=>['driver'=>'monolog','handler'=>Monolog\Handler\StreamHandler::class,'with'=>['stream'=>'php://stderr']]]];
+
+return [
+    'default' => env('LOG_CHANNEL', 'single'),
+
+    'channels' => [
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
+        ],
+
+        'stderr' => [
+            'driver' => 'monolog',
+            'handler' => Monolog\Handler\StreamHandler::class,
+            'with' => [
+                'stream' => 'php://stderr',
+            ],
+            'level' => 'debug',
+        ],
+    ],
+];
